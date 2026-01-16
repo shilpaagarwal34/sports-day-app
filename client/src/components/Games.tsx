218b3a811eb4732bad69b10a628961fbb13f1494
Shilpa Agarwal <shilpaagarwal34@users.noreply.github.com>
2026-01-16 17:14:04 +0530

    Remove unused Link imports from Games and Players components

--- client/src/components/Games.tsx
+++ client/src/components/Games.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { getGames, Game, getGamePlayers, Player, getPlayers, addPlayerToGame } from '../services/api';
 import './Games.css';
 import GamePlayerAssignment from './GamePlayerAssignment';
--- client/src/components/Players.tsx
+++ client/src/components/Players.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { getPlayers, Player } from '../services/api';
 import './Players.css';
 
